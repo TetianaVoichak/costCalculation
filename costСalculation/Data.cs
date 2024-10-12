4b95a2eb7c601d6f7c59b271e9543bbc194ecc19
Tetiana Voichak <tetiana.ozatska2012@gmail.com>
2024-10-12 21:51:11 +0200

    delete date, edit date , delete caregory

--- costСalculation/Data.cs
+++ costСalculation/Data.cs
@@ -4,6 +4,8 @@ using System.Text;
 using System.Data.SQLite;
 using System.Linq;
 using System.Data.Entity;
+using System.Resources;
+using System.Windows.Interop;
 
 namespace costСalculation
 {
@@ -73,7 +75,7 @@ namespace costСalculation
             using (var ctx = new DataContext())
             {
 
-               // var listDate = ctx.InfoForDays.Select(x => new { x.idInfo, x.Date }).ToList();
+                // var listDate = ctx.InfoForDays.Select(x => new { x.idInfo, x.Date }).ToList();
 
                 //return listDate;
 
@@ -111,8 +113,8 @@ namespace costСalculation
                     .Where(info => info.Date != DateTime.MinValue) // Исключаем записи с некорректными датами / Exclude records with incorrect dates
                     .ToList();
 
- 
-            foreach (var i in infoList)
+
+                foreach (var i in infoList)
                 {
                     listI.Add(i);
                 }
@@ -124,28 +126,121 @@ namespace costСalculation
 
 
         }
-        
 
-        public static void AddInfo(InfoForDay info )
+
+        public static void AddInfo(InfoForDay info)
         {
             InfoForDayAdditionalClass infoAdd = new InfoForDayAdditionalClass();
-           // infoAdd.idInfo = info.idInfo;
+            // infoAdd.idInfo = info.idInfo;
             infoAdd.Date = info.Date.ToString();
             infoAdd.Money = info.Money;
             infoAdd.idCategory = info.Category1.idCategory;
             //infoAdd.idCategory = info.idCategory;
 
-                using (var ctx = new DataContext())
-                {
-                    
-                        ctx.InfoForDayAdditionalClassList.Add(infoAdd);
-                        ctx.SaveChanges();
-                }
-
+            using (var ctx = new DataContext())
+            {
+                ctx.InfoForDayAdditionalClassList.Add(infoAdd);
+                ctx.SaveChanges();
             }
 
         }
+
+
+        //edit date
+        public static void EditInfo(InfoForDay info)
+        {
+            InfoForDayAdditionalClass infoEdit = new InfoForDayAdditionalClass();
+            infoEdit.Date = info.Date.ToString();
+            infoEdit.Money = info.Money;
+            infoEdit.idCategory = info.Category1.idCategory;
+
+            using (var ctx = new DataContext())
+            {
+                string date = info.Date.ToString();
+                var result = ctx.InfoForDayAdditionalClassList
+                    .Where(b => b.Date == date && b.idCategory == infoEdit.idCategory)
+                    .ToList();
+                if (result != null )
+                {   
+                    result[0].Date = info.Date.ToString();
+                    result[0].Money =  info.Money ;
+                    result[0].idCategory = info.Category1.idCategory;
+
+                    if(result.Count > 1)
+                    {
+                        for (int i = 1; i < result.Count; i++)
+                            result[i].Money = 0;
+                    }
+
+                    ctx.SaveChanges();
+                }
+            }
+        }
+
+        //delete date
+        public static void DeleteDate(InfoForDay info)
+        {
+            string dateDeleteStr = info.Date.ToString();
+            using (var ctx = new DataContext())
+            {
+
+                InfoForDayAdditionalClass infoEdit = new InfoForDayAdditionalClass();
+                infoEdit.Date = info.Date.ToString();
+                infoEdit.Money = info.Money;
+                infoEdit.idCategory = info.Category1.idCategory; ;
+             
+
+                var element = ctx.InfoForDayAdditionalClassList
+                .Where(e => e.Date == dateDeleteStr).ToList();
+                if(element!=null)
+                {
+                    foreach (var item in element)
+                        ctx.InfoForDayAdditionalClassList.Remove(item);
+                    ctx.SaveChanges();
+                }
+               
+            }
+         }
+        
+
+        //delete remove category from current day
+
+        public static void DeleteCategoryFromDate(InfoForDay info, Category category)
+        {
+            
+            using (var ctx = new DataContext())
+            {
+                
+                InfoForDayAdditionalClass infoEdit = new InfoForDayAdditionalClass();
+                string strDate = "";
+                infoEdit.Date = info.Date.ToString();
+                strDate = infoEdit.Date;
+                infoEdit.Money = info.Money;
+                infoEdit.idCategory = info.Category1.idCategory; ;
+                string msg = "";
+
+                var element = ctx.InfoForDayAdditionalClassList
+                .Where(e => e.Category1.idCategory == category.idCategory && e.Date == strDate).ToList();
+                if (element != null)
+                {
+                    foreach (var item in element)
+                        ctx.InfoForDayAdditionalClassList.Remove(item);
+                    ctx.SaveChanges();
+                }
+                else
+                {
+                    msg = "Category not found in database";
+                }
+            }
+        }
+
+
+
+
+
     }
+}
+
 
 
     
--- costСalculation/MainWindow.xaml.cs
+++ costСalculation/MainWindow.xaml.cs
@@ -33,7 +33,7 @@ namespace costСalculation
             textBox_info_current_date.Text = "Today is " + DateTime.Now.Date.ToShortDateString();
 
             foreach (var a in CATEGORYLIST)
-               comboBox_category.Items.Add(a.NameCategory);
+                comboBox_category.Items.Add(a.NameCategory);
 
 
             datePickerSetDate.SelectedDate = DateTime.Today;
@@ -68,21 +68,21 @@ namespace costСalculation
             listInfoForDayCurrent = costsOf.InfoCurrentDayListCategory(datePickerMain.SelectedDate.Value, INFOFORDAYLIST, CATEGORYLIST);
             InfoCurrentDayinForm(date, listInfoForDayCurrent);
         }
-        
-
 
         costsOfDay costsOf = new costsOfDay();
         decimal money;//a variable that stores money
 
         //check and convert text to currency format
-        void workWithMoneyFromTxt()
+        decimal workWithMoneyFromTxt(string str_money)
         {
-            string str_money = textBox_cash.Text.Trim();
+            string strMoneyTrim = str_money.Trim();
             // Заменяем точку на запятую, если точка используется в качестве десятичного разделителя
-            str_money = str_money.Replace('.', ',');
+            // Replace the period with a comma if the period is used as a decimal separator
 
-            // Пробуем преобразовать строку в decimal
-            if (decimal.TryParse(str_money, NumberStyles.Any, CultureInfo.CurrentCulture, out decimal result))
+            strMoneyTrim = strMoneyTrim.Replace('.', ',');
+
+            // Пробуем преобразовать строку в decimal / change to decimal from string
+            if (decimal.TryParse(strMoneyTrim, NumberStyles.Any, CultureInfo.CurrentCulture, out decimal result))
             {
                 money = result;
             }
@@ -92,6 +92,7 @@ namespace costСalculation
                 MessageBox.Show("Invalid number format");
                 money = 0;
             }
+            return money;
 
         }
 
@@ -100,22 +101,18 @@ namespace costСalculation
         {
             try
             {
-                WorkingWithCategories workingCategory = new WorkingWithCategories(CATEGORYLIST);
-
-                bool check = false;
-                workingCategory.ReturnCategoryMethod(comboBox_category.SelectedItem.ToString(), out check);
                 Category categForInfo;
-                categForInfo = workingCategory.ReturnCategory;
-                if (check == true)
+                if (workingWithCategoryMethod(out categForInfo, comboBox_category.SelectedItem.ToString()) == true)
                 {
-                    workWithMoneyFromTxt();
-                    if(money > 0)
-                    {
-                            InfoForDay info = new InfoForDay(datePickerSetDate.SelectedDate.Value, categForInfo,money);
-                            Data.AddInfo(info);
 
-                            LoadInfoForDay(datePickerMain.SelectedDate.Value);
-                              money = 0;
+                    workWithMoneyFromTxt(textBox_cash.Text);
+                    if (money > 0)
+                    {
+                        InfoForDay info = new InfoForDay(datePickerSetDate.SelectedDate.Value, categForInfo, money);
+                        Data.AddInfo(info);
+
+                        LoadInfoForDay(datePickerMain.SelectedDate.Value);
+                        money = 0;
                         MessageBox.Show("data saved successfully");
                     }
                     else
@@ -123,40 +120,43 @@ namespace costСalculation
                         MessageBox.Show("Invalid number format");
                         money = 0;
                     }
-  
+
                 }
                 else
                 {
-                    MessageBox.Show("Kann nicht zur Datenbank hinzugefügt werden");
+                    MessageBox.Show("Cannot be added to the database");
                 }
             }
-           catch(Exception ex)
+            catch (Exception ex)
             {
                 MessageBox.Show(ex.ToString());
             }
 
-            
+
         }
 
         List<InfoForDay> tempList;
         private decimal totalAmountForTheDay(DateTime d, List<InfoForDay> list)
         {
-           return costsOf.AmountInDay(d, list);
+            return costsOf.AmountInDay(d, list);
         }
 
         //fill in the date information in the form und show
         void fillShowInfotheDateinForm(DateTime d, List<InfoForDay> tempList)
         {
+            //???
             textBox_total_amount_this_day.Text = costsOf.AmountInDay(d, tempList).ToString();
             var result = tempList.GroupBy(p => new { p.Category1.NameCategory });
 
 
             foreach (var a in result)
                 comboBox_category_choose.Items.Add(a.Key.NameCategory);
-            textBox_total_amount.Text = tempList[comboBox_category_choose.SelectedIndex].Money.ToString();
+            //   textBox_total_amount.Text = tempList[comboBox_category_choose.SelectedIndex].Money.ToString();
+            Category cat = new Category(comboBox_category_choose.SelectedItem.ToString());
+            methodCheckDateAndCategory(d, cat);
         }
 
-       
+
 
         //method allow to know List of selected Day
         private void methodCheckDate(DateTime d)
@@ -168,11 +168,11 @@ namespace costСalculation
 
             List<Category> categ = new List<Category>();
             comboBox_category_choose.SelectedIndex = 0;
-            if(costsOf.CheckDate(d, out tempList))
+            if (costsOf.CheckDate(d, out tempList))
             {
                 fillShowInfotheDateinForm(d, tempList);
                 datePickerMain.Text = d.ToString();
-            }        
+            }
             comboBox_category_choose.SelectedIndex = 0;
         }
 
@@ -202,13 +202,13 @@ namespace costСalculation
         private void methodCheckDateAndCategory(DateTime d, Category c)
         {
             tempList = new List<InfoForDay>();
-            if (costsOf.CheckCategory(d, c,  out tempList))
+            if (costsOf.CheckCategory(d, c, out tempList))
             {
                 textBox_total_amount.Text = costsOf.AmoutByCategory(tempList, c).ToString();
             }
         }
         private void comboBox_category_choose_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {        
+        {
             try
             {
                 if (comboBox_category_choose.Items.Count > 0)
@@ -218,14 +218,14 @@ namespace costСalculation
                     methodCheckDateAndCategory(datePickerMain.SelectedDate.Value, cat);
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 MessageBox.Show(ex.ToString());
             }
         }
 
         public void UpdateInfoInComboboxCategory()
-        {  
+        {
             CATEGORYLIST = Data.GetCategories();
             comboBox_category.Items.Clear();
             foreach (var a in CATEGORYLIST)
@@ -241,10 +241,177 @@ namespace costСalculation
         }
 
 
-
         private void comboBox_category_choose_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
+
+        }
+
+        //edit current entry
+        private void button_edit_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Do you want to edit the selected date?",
+                     "Edit",
+                     MessageBoxButton.YesNo,
+                     MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                textBox_total_amount.IsEnabled = true;
+                button_delete_category.IsEnabled = true;
+                button_save_edit.IsEnabled = true;
+
+            }
+            else
+            {
+                button_delete_category.IsEnabled = false;
+                button_save_edit.IsEnabled = false;
+                textBox_total_amount.IsEnabled = false;
+            }
+        }
+
+        //determining which category is selected in order to change the data in the future / определение какая категория выбрана чтобы в будущем менять данные
+        bool workingWithCategoryMethod(out Category categForInfo, string categoryFromCombobox)
+        {
+            WorkingWithCategories workingCategory = new WorkingWithCategories(CATEGORYLIST);
+            bool check = false;
+            workingCategory.ReturnCategoryMethod(categoryFromCombobox, out check);
+            bool categoryIsAvailable = check;
+            categForInfo = workingCategory.ReturnCategory;
+            return categoryIsAvailable;
+        }
+
+        //save edited date
+        private void button_save_edit_Click(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                if (comboBox_category_choose.Items.Count == 0)
+                {
+                    MessageBox.Show("Editing is not possible, first add information about the day in the left part of the app.");
+                }
+
+                else
+                {
+                    Category categForInfo;
+                    if (workingWithCategoryMethod(out categForInfo, comboBox_category_choose.SelectedItem.ToString()) == true)
+                    {
+                        //Category category = new Category();
+                        //category = categForInfo;
+                        money = workWithMoneyFromTxt(textBox_total_amount.Text);
+                        if (money > 0)
+                        {
+                            InfoForDay info = new InfoForDay(datePickerMain.SelectedDate.Value, categForInfo, money);
+                            Data.EditInfo(info);
+                            LoadInfoForDay(datePickerMain.SelectedDate.Value);
+                            money = 0;
+                            textBox_total_amount.IsEnabled = false;
+                            button_delete_category.IsEnabled = false;
+                            button_save_edit.IsEnabled = false; 
+
+                            MessageBox.Show("changes have been saved");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Invalid number format");
+                            money = 0;
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Cannot be added to the database");
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void button_delete_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (MessageBox.Show("Do you want to delete the selected date?",
+                      "Delete",
+                      MessageBoxButton.YesNo,
+                      MessageBoxImage.Question) == MessageBoxResult.Yes)
+                {
+                    if (comboBox_category_choose.Items.Count > 0)
+                    {
+                        Category categForInfo;
+                        if (workingWithCategoryMethod(out categForInfo, comboBox_category_choose.SelectedItem.ToString()) == true)
+                        {
+                            InfoForDay info = new InfoForDay(datePickerMain.SelectedDate.Value, categForInfo, money);
+                           
+                            Data.DeleteDate(info);
+                            LoadInfoForDay(datePickerMain.SelectedDate.Value);
+                            money = 0;
+                            textBox_total_amount.IsEnabled = false;
+                            MessageBox.Show("changes have been saved");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Date does not exist!");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
             
         }
+
+        private void button_delete_category_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (MessageBox.Show("Do you want to remove the current category from the list?",
+                     "Delete  category",
+                     MessageBoxButton.YesNo,
+                     MessageBoxImage.Question) == MessageBoxResult.Yes)
+                {
+                    if (comboBox_category_choose.Items.Count > 0)
+                    {
+                        Category categForInfo;
+                        if (workingWithCategoryMethod(out categForInfo, comboBox_category_choose.SelectedItem.ToString()) == true)
+                        {
+                            money = workWithMoneyFromTxt(textBox_total_amount.Text);
+                            if (money > 0)
+                            {
+                                InfoForDay info = new InfoForDay(datePickerMain.SelectedDate.Value, categForInfo, money);
+                                Data.DeleteCategoryFromDate(info, categForInfo);
+                                LoadInfoForDay(datePickerMain.SelectedDate.Value);
+                                money = 0;
+                                textBox_total_amount.IsEnabled = false;
+                                button_delete_category.IsEnabled = false;
+                                button_save_edit.IsEnabled = false;
+                                MessageBox.Show("changes have been saved");
+                            }
+                            else
+                            {
+                                MessageBox.Show("Invalid number format");
+                                money = 0;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Category does not exist!");
+                        textBox_total_amount.IsEnabled = false;
+                        button_delete_category.IsEnabled = false;
+                        button_save_edit.IsEnabled = false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
     }
+
 }
